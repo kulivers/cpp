ad1e9b52379536e61050641f8c10ff7ec6b5ad15
coolson <alxkul@yandex.ru>
2016-10-21 15:17:05 +0300

    CopyStrings added

--- chars-examples/while-p++/while-p++/while-p++.cpp
+++ chars-examples/while-p++/while-p++/while-p++.cpp
@@ -3,6 +3,10 @@
 
 #include "stdafx.h"
 
+#define IN
+#define OUT
+#define HARDCORE
+
 bool CompareStrings(const char* p, const char* q)
 {
 	//comparing strings p and q
@@ -10,12 +14,46 @@ bool CompareStrings(const char* p, const char* q)
 	return !(*p || *q);
 }
 
+char* CopyStrings(IN const char* in, OUT char* out)
+{
+#ifdef HARDCORE
+	while (*in && *out && (*out++ = *in++));
+#else
+	while (*in && *out)
+	{
+		*out = *in;
+		if (!(*out = *in))
+			break;
+
+		*out++;
+		*in++;
+	}
+#endif
+
+	if (*out)
+		*out = 0;
+
+	return out;
+}
+
 int main()
 {
-	char* p = "Hi there";
+	char* p = new char[255] { "Hi thereeeeeeeeeeeee\0" };
 	char* q = "Hi there";
 
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = 40;
+
+	cout << (a = b = c = d) << endl;
+
+	CopyStrings(q, p);
+	cout << p << endl;
+
 	cout << "Are the strings equal? That's very " << (CompareStrings(p, q) ? "true" : "false") << endl;
+
+	delete[] p;
 
     return 0;
 }
